a9eafaa0b1665a805eda1f81002ec9e6027e26bc
Mohamed Adio <madio@fandm.edu>
2024-02-02 11:32:12 -0500

    2nd commit

--- power-digit-sum.cpp
+++ power-digit-sum.cpp
@@ -2,10 +2,11 @@
 #include <string>
 using namespace std; 
 
+//  I thought about the sum of a = 6 and b = 12 what would be the sum of the digits of
+//the value n = ab?  0 times this week >_<
 
 
-
-int power(int a, int b){
+unsigned long long int pow(unsigned int a, unsigned int b){
 
     int sum = a; 
 
@@ -14,24 +15,30 @@ int power(int a, int b){
     }else if (a == 0){ // base case for 0 base 
         return 0; 
     }else if(b == 1){ // base case for exponent  == 1 
-        return sum; 
+        return a; 
 
-    }
-
-    for(int c = b; c > 1 ; --c){
-        sum = sum * a; 
-
+    }else{
+        for(int c = b; c > 1 ; --c){
+            sum = sum * a; 
+        }
+       
     }
     return sum; 
+    
 }
 
-int sum_power(int powerSum){
+// changed my initial sum approach. I learned from shawn's appoach using mod to get the indices
+// of the sum rather than iterating and casting.
+// the mod is taking the last value and adding it to the sum. after the powersum is divide. 
 
-    string stringPowerSum = to_string(powerSum); // cast int to string to iterate
-    int sum = 0; 
+long long  sum_power(long long  powerSum){
 
-    for(int i = 0; i < stringPowerSum.length(); ++i){
-        sum += stoi(stringPowerSum.substr(i,1)); // converts the  string to int through each iteration. starts with 0 idex and the first number. if it was (i,0) it would have anything
+    long long  sum = 0; 
+
+    while(powerSum != 0 ){
+        sum += powerSum %10;
+        powerSum /= 10; 
+
     }
     
     return sum; 
@@ -46,14 +53,23 @@ int main(){
 
     cout << "Enter a "<< endl; 
     cin >> a; 
+
+    if(a < 0 ){
+        cout<< "Invalid Input" << endl; 
+    }
+
     cout << "Enter b "<< endl; 
     cin >> b; 
 
+    if(b < 0  ){
+        cout<< "Invalid Input" << endl; 
+    }
+
     cout <<  "a: " << a << endl;
     cout <<  "b: " << b << endl;
-    cout <<  a << "^"<< b << " = " << power(a,b)<< endl;
-    cout << "Sum of digits: " << sum_power(power(a,b))<<endl; 
-
+    cout <<  a << "^"<< b << " = " << pow(a,b)<< endl;
+    cout << "Sum of digits: " << sum_power(pow(a,b))<<endl; 
+    return 0; 
 
 
 
